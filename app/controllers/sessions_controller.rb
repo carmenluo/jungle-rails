5a5a6874ef9caa1ae0bba81b8bdaf667ddccac7c
Carmen <sdluojm@gmail.com>
2019-10-17 15:06:26 +0000

    add nav bar for login and signup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    # check if user'email exist
+    user = User.find_by_email(params[:email])
+    if (user) && (user.authenticate(params[:password]))
+      session[user_id] = user.id
+      redirect_to '/'
+    else
+      render :new
+    end
   end
 
   def destroy
+    session[user_id] = null
+    redirect_to '/'
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
-    if (@user.save)
+    if (user.save)
       session[:user_id] = user.id
       redirect_to '/'
     else
@@ -14,6 +14,6 @@ class UsersController < ApplicationController
   end
   private
   def user_params
-    params.require(:user).permit(:email, password, :password_confirmation)
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
