374ac9931f89423fc2c3ddfd282194496830b4c8
Carmen <sdluojm@gmail.com>
2019-10-17 14:19:17 +0000

    TODO:bugs in edit.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
-    resources :categories, only: [:create,:new, :edit, :index]
+    resources :categories, only: [:create, :new, :edit, :index]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
