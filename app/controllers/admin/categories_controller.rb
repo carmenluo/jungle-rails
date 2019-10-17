f93282ab4322c118e74c4a8189a1f4bee295de93
carmenluo <Sdluojm@gmail.com>
2019-10-16 22:57:25 -0400

    create new category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -5,4 +5,12 @@ class Admin::CategoriesController < ApplicationController
   def new
     @category = Category.new
   end
+  def create
+    @category = Category.find_or_create_by! name: params["category"]["name"]
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
 end
