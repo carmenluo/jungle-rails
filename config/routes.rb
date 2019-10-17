abfd1b173d8493f4733a4eeb762b58e1135cf2dd
Carmen <sdluojm@gmail.com>
2019-10-17 18:01:34 +0000

    add partial view for sales item

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
     resources :categories, only: [:create, :new, :edit, :index, :update]
-    resources :sales, only: [:index]
+    resources :sales, only: [:index, :new]
   end
   # Add user authentication for register and login
   # resources :users, only: [:new, :create]
