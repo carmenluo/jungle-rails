fe5c4eb8f2324d5d01495426e84e1c58a1be83a4
carmenluo <Sdluojm@gmail.com>
2019-10-16 21:36:06 -0400

    initialize new and new.html.erb

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -2,4 +2,7 @@ class Admin::CategoriesController < ApplicationController
   def index
     @categories = Category.all.order(created_at: :desc)
   end
+  def new
+    @category = Category.new
+  end
 end
