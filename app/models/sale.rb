10ed69d08d04310fd80d7cbb7077c27c07fbfae0
Carmen <sdluojm@gmail.com>
2019-10-18 11:30:08 +0000

    show sales with business logic to determine if it is active

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,2 +1,19 @@
 class Sale < ActiveRecord::Base
+  # AR Scope - class method with seld implicit
+  def self.active
+    where("starts_on <= ? AND ends_on >= ?", Date.current, Date.current)
+  end
+  def self.finished
+    where("ends_on < ?", Date.current).any?
+  end
+  def finished?
+    ends_on < Date.current
+  end
+  def upcoming?
+    starts_on > Date.current
+  end
+  def active?
+    !upcoming? && !finished?
+  end
+
 end
