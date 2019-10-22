b2bd969eee7ae6fd982574e14db8b8bafd2fb1f2
Carmen <sdluojm@gmail.com>
2019-10-22 00:06:48 +0000

    fix product test case

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,9 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-
-    it 'should validates :name, presence: true' do
-      cat1 = Category.find_or_create_by! name: 'Apparel'
+    before do
+      cat1 = Category.find_or_create_by! name: 'Apparel'    
       @product = Product.create!({
         name:  'Men\'s Classy shirt',
         description: 'description',
@@ -12,47 +11,26 @@ RSpec.describe Product, type: :model do
         price: 64.99,
         category: cat1
       })
+    end
+  
+    it 'should validates :name, presence: true' do 
       expect(@product.name).to be_present
       expect(@product.price).to be_present
       expect(@product.quantity).to be_present
       expect(@product.category).to be_present
     end
     it 'should validates :description, presence: true' do
-      cat1 = Category.find_or_create_by! name: 'Apparel'
-      @product = Product.create!({
-        name:  'Men\'s Classy shirt',
-        description: 'description',
-        quantity: 10,
-        price: 64.99,
-        category: cat1
-      })     
       expect(@product.price).to be_present
     end
     it 'should validates :quantity, presence: true' do
-      cat1 = Category.find_or_create_by! name: 'Apparel'
-      @product = Product.create!({
-        name:  'Men\'s Classy shirt',
-        description: 'description',
-        quantity: 10,
-        price: 64.99,
-        category: cat1
-      })
       expect(@product.quantity).to be_present
     end
     it 'should validates :category, presence: true' do
-      cat1 = Category.find_or_create_by! name: 'Apparel'
-      @product = Product.create!({
-        name:  'Men\'s Classy shirt',
-        description: 'description',
-        quantity: 10,
-        price: 64.99,
-        category: cat1
-      })
       expect(@product.category).to be_present
     end
     it 'should validates :fields, presence: nil' do
       cat1 = Category.find_or_create_by! name: 'Apparel'
-      @product = Product.create!({
+      @product = Product.new({
         description: 'description',
         quantity: 10,
         price: 64.99,
@@ -63,7 +41,7 @@ RSpec.describe Product, type: :model do
           puts message
         end
       end
-      expect(@product.name).to be_nill
+      expect(@product).to_not be_valid
 
     end
   end
